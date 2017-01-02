5dd1ca44433f2844193e781d6682ffab5ca05fd6
Peter Czibik <p.czibik@gmail.com>
2017-01-02 23:13:23 +0100

    add package control script

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+(require 'cfg-package)
+
+(peteyy-ensure-package-installed 'magit)
+
+(global-set-key (kbd "C-x C-g") 'magit-status)
+
 (when (eq window-system 'mac)
   (peteyy-mac-mode-hook))
 
@@ -5,29 +11,5 @@
   (setq mac-command-modifier 'meta)
   (setq mac-option-modifier 'meta))
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
-(defun ensure-package-installed (&rest packages)
-  "Assure every package is installed, ask for installation if it’s not.
 
-Return a list of installed packages or nil for every skipped package."
-  (mapcar
-   (lambda (package)
-     ;; (package-installed-p 'evil)
-     (if (package-installed-p package)
-         nil
-       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
-           (package-install package)
-         package)))
-   packages))
-
-;; make sure to have downloaded archive description.
-;; Or use package-archive-contents as suggested by Nicolas Dudebout
-(or (file-exists-p package-user-dir)
-    (package-refresh-contents))
-
-(ensure-package-installed 'magit) ;  --> (nil nil) if iedit and magit are already installed
-
-;; activate installed packages
-(package-initialize)
