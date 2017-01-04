c97c36d73e68e6a555a2035d84bf84d4058e899e
Peter Czibik <p.czibik@gmail.com>
2017-01-04 21:02:37 +0100

    Move autosave files and add whole-line-or-region

--- init.el
+++ init.el
@@ -8,7 +8,8 @@
  'magit
  'projectile
  'smex
- 'ido-vertical-mode)
+ 'ido-vertical-mode
+ 'whole-line-or-region)
 
 ;; Ido vertical mode works better for me
 (require 'ido-vertical-mode)
@@ -19,11 +20,19 @@
 ;; Disable toolbar
 (tool-bar-mode -1)
 
+;; Cut and copy whole line or region mode
+(whole-line-or-region-mode 1)
+
+;; Projectile mode
+(projectile-mode 1)
+
 ;; Configure backup file creation in it's own directory
 (defvar peteyy/backup-directory (concat user-emacs-directory "backups"))
 (unless (file-exists-p peteyy/backup-directory)
   (make-directory peteyy/backup-directory t))
 (setq backup-directory-alist `(("." . ,peteyy/backup-directory))
+      auto-save-file-name-transforms
+          `(("." ,peteyy/backup-directory t))
       make-backup-files t
       version-control t
       backup-by-copying-when-linked t
@@ -37,6 +46,20 @@
 
 ;; Change customize-* file
 (setq custom-file (concat user-emacs-directory "custom.el"))
+
+;; Smart C-a
+(defun smart-beginning-of-line ()
+  "Move point to first non-whitespace character or beginning-of-line.
+
+Move point to the first non-whitespace character on this line.
+If point was already at that position, move point to beginning of line."
+  (interactive)
+  (let ((oldpos (point)))
+    (back-to-indentation)
+    (and (= oldpos (point))
+         (beginning-of-line))))
+(global-set-key [home] 'smart-beginning-of-line)
+(global-set-key "\C-a" 'smart-beginning-of-line)
 
 (defun peteyy/mac-mode-hook ()
   (setq mac-command-modifier 'meta
