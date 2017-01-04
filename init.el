0deea25439cc04b537089e92d00a9dd7c7681efc
Peter Czibik <p.czibik@gmail.com>
2017-01-04 22:10:14 +0100

    Refactor to use use-package

--- init.el
+++ init.el
@@ -2,47 +2,51 @@
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-(require 'cfg-package)
-
-(peteyy/ensure-package-installed
- 'magit
- 'projectile
- 'smex
- 'ido-vertical-mode
- 'whole-line-or-region)
-
-;; Ido vertical mode works better for me
-(require 'ido-vertical-mode)
-(ido-mode 1)
-(ido-vertical-mode 1)
-(setq ido-vertical-define-keys 'C-n-and-C-p-only)
-
-;; Disable toolbar
+;; Remove cluttered toolbar
 (tool-bar-mode -1)
 
-;; Cut and copy whole line or region mode
-(whole-line-or-region-mode 1)
+;; Nice built-in completion-system
+(ido-mode 1)
 
-;; Projectile mode
-(projectile-mode 1)
+;; Disable autosave because it's slow
+(auto-save-mode -1)
+
+;; Git integration
+(use-package magit
+  :ensure t
+  :bind ("C-x C-g" . magit-status))
+
+;; Manage projects with a keystroke
+(use-package projectile
+  :ensure t
+  :config (projectile-mode 1))
+
+;; Ido flavored M-x
+(use-package smex
+  :ensure t
+  :bind (("M-x" . smex)))
+
+;; Vertical alignment for ido
+(use-package ido-vertical-mode
+  :ensure t
+  :init (setq ido-vertical-define-keys 'C-n-and-C-p-only)
+  :config (ido-vertical-mode 1))
+
+;; If nothing is marked yanks whole line
+(use-package whole-line-or-region
+  :ensure t
+  :config (whole-line-or-region-mode 1))
 
 ;; Configure backup file creation in it's own directory
 (defvar peteyy/backup-directory (concat user-emacs-directory "backups"))
 (unless (file-exists-p peteyy/backup-directory)
   (make-directory peteyy/backup-directory t))
 (setq backup-directory-alist `(("." . ,peteyy/backup-directory))
-      auto-save-file-name-transforms
-          `(("." ,peteyy/backup-directory t))
       make-backup-files t
       version-control t
       backup-by-copying-when-linked t
       delete-old-versions t
-      delete-by-moving-to-trash t
-      auto-save-default t)
-
-;; Custom keybindings
-(global-set-key (kbd "C-x C-g") 'magit-status)
-(global-set-key (kbd "M-x") 'smex)
+      delete-by-moving-to-trash t)
 
 ;; Change customize-* file
 (setq custom-file (concat user-emacs-directory "custom.el"))
